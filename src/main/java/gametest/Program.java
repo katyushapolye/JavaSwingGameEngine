4b32cc809597094480709c8d979804d969147dac
Raphael Laroca <raphael.laroca@gmail.com>
2022-05-19 15:14:11 -0300

    Push Test

--- src/main/java/gametest/Program.java
+++ src/main/java/gametest/Program.java
@@ -10,7 +10,7 @@ public class Program {
 		GameObject debugObject =  new GameObject("src/main/resources/Sprite_Sheet.png",-800,-800);
 		GameObject debugObject2 =  new GameObject("src/main/resources/Sprite_Sheet.png",0,-400);
 		GameObject[] gmBuffer =  new GameObject[2];
-		//gmBuffer[0] = debugObject;
+		gmBuffer[0] = debugObject;
 		gmBuffer[1] = debugObject2;
 		
 		long tickStart = clock.millis();
