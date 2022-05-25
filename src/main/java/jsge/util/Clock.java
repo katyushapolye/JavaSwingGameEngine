49a37055f7eff4016590b8d30109afbe5360e8ac
Raphael Laroca <raphael.laroca@gmail.com>
2022-05-25 13:57:07 -0300

    Pushed the clock class

---
+++ src/main/java/jsge/util/Clock.java
@@ -0,0 +1,24 @@
+package jsge.util;
+
+public class Clock {
+	long epoch = 0;
+	public Clock() {
+		epoch = System.nanoTime();
+	}
+	
+	public long getElapsedTimeInNanoSeconds() {
+		return System.nanoTime() - epoch;
+	}
+	
+	public double getElapsedTimeInSeconds() {
+		return (System.nanoTime()- epoch)/1000000000;
+	}
+	
+	
+	public double resetClock() {
+		long temp = epoch;
+		epoch = System.nanoTime();
+		return (epoch - temp)/(double)1000000000;
+	}
+
+}
