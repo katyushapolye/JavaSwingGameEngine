0009d28ebd8950fb401a42eb3d0055e6453b6d0b
Raphael Laroca <raphael.laroca@gmail.com>
2022-06-05 11:23:59 -0300

    Implementation on binary audio buffer for optimization

--- src/main/java/jsge/data/AudioClip.java
+++ src/main/java/jsge/data/AudioClip.java
@@ -2,8 +2,10 @@ package jsge.data;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -15,67 +17,110 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.sound.sampled.LineEvent.Type;
 
 public class AudioClip {
-	
+
 	String audioName;
 	AudioInputStream input;
-	AudioInputStream def;
-	byte[] audioBuffer =  new byte[10000000];
 	Clip audioClip;
+	File audioFile;
+
+	byte[] wavHeader =
+			// Intel's little endian hex notation
+			{ 82, 73, 70, 70, // RIFF STRING
+					00, 00, 00, 00, // Filesize - To be set later along with datasize
+					87, 65, 86, 69, // Wav ascii string
+					102, 109, 116, 32, // "fmt " trailing space
+					16, 00, 00, 00, // wav flags
+					01, 00, 01, 00, // wav flags
+					34, 86, 00, 00,
+					34, 86, 00, 00,
+					01, 00, 8, 00,
+					100, 97, 116, 97,
+					00, 00, 00, 00, // data size
+			// start data
+			};
+
+	byte[] wavData;
 	
+	byte[] audioBuffer;
+
 	private String pathToFile;
-	
+
 	public AudioClip(String pathToFile) {
 		this.pathToFile = pathToFile;
 		try {
-		input = AudioSystem.getAudioInputStream(new File(pathToFile));
-		//audioBuffer = input.readAllBytes();
-		//def = AudioSystem.getAudioInputStream(new ByteArrayInputStream(audioBuffer));
-		audioClip = AudioSystem.getClip();
-		
-		audioClip.addLineListener(new LineListener() {
-			
-			@Override
-			public void update(LineEvent event) {
-				if(event.getType().equals(Type.STOP)){
-					System.out.println("END");
-					audioClip.drain();
-					audioClip.flush();
-					audioClip.close();
+			audioFile = new File(pathToFile);
+		} catch (Exception e) {
+			System.out.println("AudioClip: Warning - Failed to load audio file " + pathToFile);
 		}
-				
+		try {
+			
+			//Creating binary data
+			input = AudioSystem.getAudioInputStream(audioFile);
+			wavData = input.readAllBytes();
+			int wavDataLenght = wavData.length;
+			byte[] filesize = ByteBuffer.allocate(4).putInt(Integer.reverseBytes(wavDataLenght + 36)).array();
+
+			for (int i = 0; i < 4; i++) {
+				wavHeader[4 + i] = filesize[i];
 			}
-		});
-		}
-		catch(Exception e) {
-			System.out.println("AudioClip: Warning - The clip has failed to load.");
+			filesize = ByteBuffer.allocate(4).putInt(Integer.reverseBytes(wavDataLenght)).array();
+			for (int i = 0; i < 4; i++) {
+				wavHeader[40 + i] = filesize[i];
+			}
+			audioBuffer = new byte[44+wavDataLenght];
 			
 			
+			System.arraycopy(wavHeader,0,audioBuffer, 0, 44);
+			System.arraycopy(wavData,0, audioBuffer,44, wavDataLenght);
+
+
+			audioClip = AudioSystem.getClip();
+
+			audioClip.addLineListener(new LineListener() {
+
+				@Override
+				public void update(LineEvent event) {
+					if (event.getType().equals(Type.STOP)) {
+						System.out.println("END");
+						audioClip.drain();
+						audioClip.flush();
+						audioClip.close();
+					}
+
+				}
+			});
+		} catch (Exception e) {
+			System.out.println("AudioClip: Warning - The clip has failed to load the audio data");
+
 			return;
 		}
 	}
-	
-	//testing
-	
-	public void playTest(){
-		if(audioClip.isOpen()) {
+
+	// testing
+
+	public void playTest() {
+		if (audioClip.isOpen()) {
 			return;
 		}
-		
+
 		try {
 			resetAudioStream();
 			audioClip.open(input);
 			audioClip.start();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
-		
+
 	}
-	
+
 	private void resetAudioStream() throws UnsupportedAudioFileException, IOException {
-		//Try to fit everythinh into byte array, resetting the stream losses audio quality ???? and loading a new file everytime is wasteful
-		this.input.reset();
+		// Try to fit everythinh into byte array, resetting the stream losses audio
+		// quality ???? and loading a new file everytime is wasteful
+		
+		input = AudioSystem.getAudioInputStream(new ByteArrayInputStream(audioBuffer));
+		//input = AudioSystem.getAudioInputStream(audioFile);
+		
 	}
 
 }
